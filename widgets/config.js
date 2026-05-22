f612e063963e8ab82e269ada1404d0bd9c7d6d9d
satheeshpotti <msreekutty191@gmail.com>
2026-05-22 20:24:52 +0530

    Update config.js

--- widgets/config.js
+++ widgets/config.js
@@ -6,7 +6,7 @@ window.FINANCE_CONFIG = {
     Settings: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=1327943434&single=true&output=csv", 
     Categories: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=197316364&single=true&output=csv", 
     Accounts: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=511010124&single=true&output=csv", 
-    Transactions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=1409427928&single=true&output=csv", 
+    Transactions: "https://www.notion.so/368160e7ca1180a68866d8c045fc1eff?v=368160e7ca118012b54a000c182b5639", 
     Budgets: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=1060083035&single=true&output=csv", 
     Recurring: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=1375853836&single=true&output=csv", 
     Debts: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIA7aYkZ0Xjk0U8JJGTkg4cZIp_Qggx0biJAgRa3kFNqTN_5Um91VSYjpMqlH8A5d3LP9P_2t0qg23/pub?gid=96100523&single=true&output=csv", 
