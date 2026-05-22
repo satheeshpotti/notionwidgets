4a2951a0d20529173dccf4de44c2769332d34aaa
satheeshpotti <msreekutty191@gmail.com>
2026-05-22 14:17:48 +0530

    premium finance widgets

---
+++ widgets/config.js
@@ -0,0 +1,7 @@
+window.FINANCE_CONFIG = {
+  dataMode: "embedded",
+  csvBase: "csv_data_source",
+  autoRefreshMinutes: 10,
+  googleSheetsCsv: { Settings:"", Categories:"", Accounts:"", Transactions:"", Budgets:"", Recurring:"", Debts:"", Goals:"", Subscriptions:"", Wishlist:"", Bills:"", Orders:"", NoSpend:"" },
+  githubRawBase: ""
+};
