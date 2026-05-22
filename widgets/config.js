be1a3ea429c21efce457e54328d819e6ad2e518e
satheeshpotti <msreekutty191@gmail.com>
2026-05-22 18:15:47 +0530

    Updated finance dashboard

--- widgets/config.js
+++ widgets/config.js
@@ -1,7 +1,21 @@
 window.FINANCE_CONFIG = {
-  dataMode: "embedded",
+  dataMode: "googleSheets",
   csvBase: "csv_data_source",
   autoRefreshMinutes: 10,
-  googleSheetsCsv: { Settings:"", Categories:"", Accounts:"", Transactions:"", Budgets:"", Recurring:"", Debts:"", Goals:"", Subscriptions:"", Wishlist:"", Bills:"", Orders:"", NoSpend:"" },
+  googleSheetsCsv: {
+    Settings: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1327943434#gid=1327943434", 
+    Categories: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=197316364#gid=197316364", 
+    Accounts: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=511010124#gid=511010124", 
+    Transactions: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1409427928#gid=1409427928", 
+    Budgets: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1060083035#gid=1060083035", 
+    Recurring: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1375853836#gid=1375853836", 
+    Debts: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=96100523#gid=96100523", 
+    Goals: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1048878317#gid=1048878317", 
+    Subscriptions: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=828550228#gid=828550228", 
+    Wishlist: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=155840862#gid=155840862", 
+    Bills: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=85482757#gid=85482757", 
+    Orders: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=938123705#gid=938123705", 
+    NoSpend: "https://docs.google.com/spreadsheets/d/1QyNATS05nF6fuyOiMlV2LgFmOtv4V__3v3keuwkhV9M/edit?gid=1999922847#gid=1999922847"
+  },
   githubRawBase: ""
 };
